1a00df35671810e80d1fca21a741f0f5d9dbd3ab
Leonardo <leobarros_@outlook.com>
2026-02-02 20:18:36 -0300

    chore: initial setup

---
+++ src/main/java/leonardo/payment_management_system/PaymentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package leonardo.payment_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leonardo/payment_management_system/PaymentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardo.payment_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
