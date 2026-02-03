d75a313ff8c29a10888890193444812a3896964d
Leonardo <leobarros_@outlook.com>
2026-02-02 23:44:00 -0300

    refactor: remove set id from paymentDTO

--- src/main/java/leonardo/payment_management_system/dto/payment/PaymentDTO.java
+++ src/main/java/leonardo/payment_management_system/dto/payment/PaymentDTO.java
@@ -28,10 +28,6 @@ public class PaymentDTO {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public BigDecimal getValue() {
         return value;
     }
