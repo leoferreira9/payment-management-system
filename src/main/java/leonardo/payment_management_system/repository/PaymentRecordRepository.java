ed23493f668e6e52b5af1f0d3f5648a19b19178f
Leonardo <leobarros_@outlook.com>
2026-02-04 23:19:30 -0300

    feat: add find all query to payment record repository

--- src/main/java/leonardo/payment_management_system/repository/PaymentRecordRepository.java
+++ src/main/java/leonardo/payment_management_system/repository/PaymentRecordRepository.java
@@ -3,4 +3,8 @@ package leonardo.payment_management_system.repository;
 import leonardo.payment_management_system.entity.PaymentRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRecordRepository extends JpaRepository<PaymentRecord, Long> {}
+import java.util.List;
+
+public interface PaymentRecordRepository extends JpaRepository<PaymentRecord, Long> {
+    List<PaymentRecord> findAllByPaymentId(Long id);
+}
