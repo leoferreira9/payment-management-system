e2d10a017a5fbf24b75f9b5b4efc11f5c9af4068
Leonardo <leobarros_@outlook.com>
2026-02-04 23:27:10 -0300

    feat: add payment record service

---
+++ src/main/java/leonardo/payment_management_system/service/PaymentRecordService.java
@@ -0,0 +1,63 @@
+package leonardo.payment_management_system.service;
+
+import leonardo.payment_management_system.dto.paymentRecord.CreatePaymentRecordDTO;
+import leonardo.payment_management_system.dto.paymentRecord.PaymentRecordDTO;
+import leonardo.payment_management_system.entity.Payment;
+import leonardo.payment_management_system.entity.PaymentRecord;
+import leonardo.payment_management_system.enums.PaymentRecordStatus;
+import leonardo.payment_management_system.enums.PaymentStatus;
+import leonardo.payment_management_system.exception.EntityNotFound;
+import leonardo.payment_management_system.mapper.PaymentRecordMapper;
+import leonardo.payment_management_system.repository.PaymentRecordRepository;
+import leonardo.payment_management_system.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+public class PaymentRecordService {
+
+    private final PaymentRecordRepository paymentRecordRepository;
+    private final PaymentRepository paymentRepository;
+    private final PaymentRecordMapper mapper;
+
+    public PaymentRecordService(PaymentRecordRepository paymentRecordRepository, PaymentRecordMapper mapper, PaymentRepository paymentRepository){
+        this.paymentRecordRepository = paymentRecordRepository;
+        this.paymentRepository = paymentRepository;
+        this.mapper = mapper;
+    }
+
+    public PaymentRecord findPaymentRecordOrThrow(Long id){
+        return paymentRecordRepository.findById(id).orElseThrow(() -> new EntityNotFound("Payment record not found with ID: " + id));
+    }
+
+    private static final Map<PaymentRecordStatus, PaymentStatus> map = new EnumMap<>(PaymentRecordStatus.class);
+
+    static {
+        map.put(PaymentRecordStatus.PAID, PaymentStatus.PAID);
+        map.put(PaymentRecordStatus.CANCELLED,PaymentStatus.CANCELLED);
+        map.put(PaymentRecordStatus.REFUNDED, PaymentStatus.CANCELLED);
+    }
+
+    public PaymentRecordDTO create(CreatePaymentRecordDTO dto){
+        Payment payment = paymentRepository.findById(dto.getPaymentId()).orElseThrow(() -> new EntityNotFound("Payment not found with ID: " + dto.getPaymentId()));
+        PaymentRecord paymentRecord = mapper.toEntity(dto);
+
+        paymentRecord.setValue(payment.getValue());
+        paymentRecord.setPayment(payment);
+        paymentRecord.setPaymentType(payment.getPaymentType());
+        paymentRecord.setPaymentDeadlineSnapshot(payment.getPaymentDeadline());
+        paymentRecord.setEventDate(LocalDateTime.now());
+
+        PaymentRecord savedPaymentRecord = paymentRecordRepository.save(paymentRecord);
+        payment.setStatus(map.get(dto.getStatus()));
+        paymentRepository.save(payment);
+
+        return mapper.toDto(savedPaymentRecord);
+    }
+
+    public List<PaymentRecordDTO> findAllByPaymentId(Long id){
+        return paymentRecordRepository.findAllByPaymentId(id).stream().map(mapper::toDto).toList();
+    }
+}
